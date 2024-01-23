454c8f333fd39ce4186d6f7e61a715a2110de81e
AppleOngPS <ongapple1@gmail.com>
2024-01-23 17:47:11 +0800

    Add API URL and Key

---
+++ js/challenge2.js
@@ -0,0 +1,10 @@
+//Add API Link and Key
+const url =
+  "https://genius-song-lyrics1.p.rapidapi.com/artist/albums/?id=344497&per_page=20&page=1";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f69c30f9d6msh672713a3ae759b0p188a2djsn8ea3363cbb73",
+    "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+  },
+};
