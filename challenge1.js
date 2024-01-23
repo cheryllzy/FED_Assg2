f060366fdd0ff0cd36cf374b49cf67d50439071e
AppleOngPS <ongapple1@gmail.com>
2024-01-23 16:46:09 +0800

    Create FetchData Function

--- challenge1.js
+++ challenge1.js
@@ -8,3 +8,28 @@ const apiOptions = {
     "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
   },
 };
+
+let chorusText = ""; // Variable to store the Chorus text
+let correctAnswers = ["faded"]; //Correct answer to the blank
+async function fetchData() {
+  try {
+    const response = await fetch(apiUrl, apiOptions);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    const lyrics = result.lyrics.lyrics.body.html;
+
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = lyrics;
+
+    const chorusElement = tempElement.querySelector('[data-id="8803037"]');
+    chorusText = chorusElement ? chorusElement.innerText : "Chorus not found";
+
+    document.getElementById("chorusLyrics").innerText = chorusText;
+  } catch (error) {
+    console.error(error);
+  }
+}
