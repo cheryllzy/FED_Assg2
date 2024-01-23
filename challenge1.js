9cd06a489292ec3da21b92f70ec45cd2f883e1c1
AppleOngPS <ongapple1@gmail.com>
2024-01-23 16:19:40 +0800

    Create Js file , Add API Key and URL

---
+++ challenge1.js
@@ -0,0 +1,10 @@
+//Add Api URL and Key
+const apiUrl =
+  "https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=2396871";
+const apiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f69c30f9d6msh672713a3ae759b0p188a2djsn8ea3363cbb73",
+    "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+  },
+};
