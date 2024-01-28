1866dae0a85c6d8886661cb2f306546443994c49
AppleOngPS <ongapple1@gmail.com>
2024-01-28 15:48:36 +0800

    Testing on the timeout duration

--- js/transition.js
+++ js/transition.js
@@ -3,4 +3,4 @@ console.log("Need to wait");
 setTimeout(function () {
   console.log("setting window location");
   window.location.href = "https://www.np.edu.sg"; //change the website to our own local page
-}, 5000); // Change the timeout duration to 5000 milliseconds (5 seconds)
+}, 3000); // Change the timeout duration to 3000 milliseconds (3 seconds)
